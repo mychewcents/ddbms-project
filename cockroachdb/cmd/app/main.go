aee0c4e8ad79867a9716cd4a91fd87c9604e308e
grey-matter <aravindkm95@gmail.com>
2020-10-21 12:50:05 +0530

    minor change

--- cockroachdb/cmd/app/main.go
+++ cockroachdb/cmd/app/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"github.com/mychewcents/ddbms-project/cockroachdb/internal/cdbconn"
 	"github.com/mychewcents/ddbms-project/cockroachdb/internal/payment"
+	"github.com/mychewcents/ddbms-project/cockroachdb/internal/order_status"
+	"github.com/mychewcents/ddbms-project/cockroachdb/internal/top_balance"
 )
 
 var db *sql.DB
@@ -22,7 +24,7 @@ func main() {
 	for true {
 
 	var transaction_type byte
-	_, err := fmt.Scanf("%c,", &transaction_type)
+	_, err := fmt.Scanf("%c", &transaction_type)
 
 	if err != nil {
 		if err.Error() == "EOF" {
@@ -38,14 +40,20 @@ func main() {
 		case 'P':
 			var warehouseId, districtId, customerId int
 				var amount float64
-			fmt.Scanf("%d,%d,%d,%f", &warehouseId, &districtId, &customerId, &amount)
+			fmt.Scanf(",%d,%d,%d,%f", &warehouseId, &districtId, &customerId, &amount)
 			payment.ProcessTransaction(db, warehouseId, districtId, customerId, amount)
 			break
 		case 'D':
 		case 'O':
+			var warehouseId, districtId, customerId int
+			fmt.Scanf(",%d,%d,%d", &warehouseId, &districtId, &customerId)
+			order_status.ProcessTransaction(db, warehouseId, districtId, customerId)
+			break
 		case 'S':
 		case 'I':
 		case 'T':
+			top_balance.ProcessTransaction(db)
+			break
 		case 'R':
 
 		}
